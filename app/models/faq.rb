18aa258fffa058319b13c9cd7bce3987539168a1
pclowes <peter.d.clowes@gmail.com>
2014-10-15 16:27:57 -0600

    added class method all to return array of all FAQs

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,7 +1,15 @@
 class Faq
+  @@allfaq = Array.new
+  def self.all
+    @@allfaq
+  end
+
   attr_reader :question, :answer
-  def initialize(question, answer)
+
+  def initialize(question=nil, answer=nil)
     @question = question
     @answer = answer
+    @@allfaq << self
   end
+
 end
