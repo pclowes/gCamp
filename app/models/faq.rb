9ad1d10ece3b3ba302e7723ee4c1d4888990c1a4
pclowes <peter.d.clowes@gmail.com>
2014-10-17 19:26:41 -0600

    fully functional faq model pull from yaml file

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -14,4 +14,11 @@ class Faq
     @answer = answer
     @@allfaq << self
   end
+
+Faq.new(CONFIG[:what][:question],CONFIG[:what][:answer])
+Faq.new(CONFIG[:how][:question],CONFIG[:how][:answer])
+Faq.new(CONFIG[:when1][:question],CONFIG[:when1][:answer])
+Faq.new(CONFIG[:scared][:question],CONFIG[:scared][:answer])
+Faq.new(CONFIG[:alternatives][:question],CONFIG[:alternatives][:answer])
+
 end
