83eea86bae18d282b459c0f3eefc5af883c5cbd8
pclowes <peter.d.clowes@gmail.com>
2014-10-18 11:56:55 -0600

    Made FAQs sortable by downcase of question with comparable in the model

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -4,6 +4,7 @@ class Faq
     @@allfaq
   end
 
+  include Comparable
   attr_reader :question, :answer
   def slug
     question.downcase.gsub(/\W+/, '')
@@ -15,10 +16,14 @@ class Faq
     @@allfaq << self
   end
 
-Faq.new(CONFIG[:what][:question],CONFIG[:what][:answer])
-Faq.new(CONFIG[:how][:question],CONFIG[:how][:answer])
-Faq.new(CONFIG[:when1][:question],CONFIG[:when1][:answer])
-Faq.new(CONFIG[:scared][:question],CONFIG[:scared][:answer])
-Faq.new(CONFIG[:alternatives][:question],CONFIG[:alternatives][:answer])
+  def <=> other
+    self.question <=> other.question
+  end
+
+Faq.new(CONFIG[:what][:question],CONFIG[:what][:answer]).slug
+Faq.new(CONFIG[:how][:question],CONFIG[:how][:answer]).slug
+Faq.new(CONFIG[:when1][:question],CONFIG[:when1][:answer]).slug
+Faq.new(CONFIG[:scared][:question],CONFIG[:scared][:answer]).slug
+Faq.new(CONFIG[:alternatives][:question],CONFIG[:alternatives][:answer]).slug
 
 end
