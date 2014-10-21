b9b852af59cd72e77c8ba902fb5c5cef7bb06218
pclowes <peter.d.clowes@gmail.com>
2014-10-21 09:15:53 -0600

    commit pre-branch

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,29 +1,30 @@
 class Faq
-  @@allfaq = Array.new
-  def self.all
-    @@allfaq
-  end
 
-  include Comparable
-  attr_reader :question, :answer
-  def slug
-    question.downcase.gsub(/\W+/, '')
-  end
+    @@allfaq = Array.new
+    def self.all
+      @@allfaq
+    end
 
-  def initialize(question, answer)
-    @question = question
-    @answer = answer
-    @@allfaq << self
-  end
+    include Comparable
+    attr_reader :question, :answer
+    def slug
+      question.downcase.gsub(/\W+/, '')
+    end
 
-  def <=> other
-    self.question <=> other.question
-  end
+    def initialize(question, answer)
+      @question = question
+      @answer = answer
+      @@allfaq << self
+    end
 
-Faq.new(CONFIG[:what][:question],CONFIG[:what][:answer]).slug
-Faq.new(CONFIG[:how][:question],CONFIG[:how][:answer]).slug
-Faq.new(CONFIG[:when1][:question],CONFIG[:when1][:answer]).slug
-Faq.new(CONFIG[:scared][:question],CONFIG[:scared][:answer]).slug
-Faq.new(CONFIG[:alternatives][:question],CONFIG[:alternatives][:answer]).slug
+    def <=> other
+      self.question <=> other.question
+    end
+
+  Faq.new(CONFIG[:what][:question],CONFIG[:what][:answer]).slug
+  Faq.new(CONFIG[:how][:question],CONFIG[:how][:answer]).slug
+  Faq.new(CONFIG[:when1][:question],CONFIG[:when1][:answer]).slug
+  Faq.new(CONFIG[:scared][:question],CONFIG[:scared][:answer]).slug
+  Faq.new(CONFIG[:alternatives][:question],CONFIG[:alternatives][:answer]).slug
 
 end
