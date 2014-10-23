0ef742f9d52dfa436118501039efeaeca89f42c6
pclowes <peter.d.clowes@gmail.com>
2014-10-23 14:44:13 -0600

    added all and complete filters to task page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,13 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    if params[:task_sort] == "All"
+      @tasks = Task.all
+    elsif params[:task_sort] == "Incomplete"
+      @tasks = Task.where(complete: false)
+    else
+      @tasks = Task.where(complete: false)
+    end
   end
 
   # GET /tasks/1
