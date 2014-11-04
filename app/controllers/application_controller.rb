2c6afa9de920ba973e4962bbfe8fe5539cd498ea
pclowes <peter.d.clowes@gmail.com>
2014-11-03 21:25:59 -0700

    moved current_user to a helper module for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
-
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
-
-
-  helper_method :current_user
-
+  include ApplicationHelper
   protect_from_forgery with: :exception
 end
