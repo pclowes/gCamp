b5d2d25ba3fa7beb7d4eb08c1da1dcddc447765e
pclowes <peter.d.clowes@gmail.com>
2014-11-04 15:41:20 -0700

    added capybara to signin and expanded signup

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -1,17 +1,38 @@
 require 'rails_helper'
 
 feature "Signin" do
-  scenario "User signs in"
+  scenario "User signs in" do
     User.create!(
-      first_name: "Foo",
+      first_name: "Foo2",
       last_name: "11/11/13",
-      email: "foo@bar.com",
+      email: "foo2@bar.com",
       password: "test"
     )
     visit root_path
     click_on "Sign In"
-    fill_in "Email", with: "foo@bar.com"
+    fill_in "Email", with: "foo2@bar.com"
     fill_in "Password", with: "test"
-    click_on "Sign In"
+    within(".well") do
+      click_on("Sign In")
+    end
+  end
+end
 
+feature "Signin" do
+  scenario "User signs in bad password" do
+    User.create!(
+      first_name: "Foo2",
+      last_name: "11/11/13",
+      email: "foo2@bar.com",
+      password: "test"
+    )
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: "foo2@bar.com"
+    fill_in "Password", with: "fest"
+    within(".well") do
+      click_on("Sign In")
+    end
+    expect(page).to have_content("Username / password combination is invalid")
+  end
 end
--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -14,3 +14,51 @@ feature "Signups" do
     end
   end
 end
+
+feature "Signups" do
+  scenario "User signs up no email" do
+    visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Foo"
+    fill_in "Last name", with: "Bar"
+    fill_in "Email", with: ""
+    fill_in "Password", with: "test"
+    fill_in "Password confirmation", with: "test"
+    within(".well") do
+      click_on("Sign Up")
+    end
+    expect(page).to have_content("Email can't be blank")
+  end
+end
+
+feature "Signups" do
+  scenario "User signs up no passwords" do
+    visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Foo"
+    fill_in "Last name", with: "Bar"
+    fill_in "Email", with: "foo@bar.com"
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    within(".well") do
+      click_on("Sign Up")
+    end
+    expect(page).to have_content("Password can't be blank")
+  end
+end
+
+feature "Signups" do
+  scenario "User signs up mismatched passwords" do
+    visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Foo"
+    fill_in "Last name", with: "Bar"
+    fill_in "Email", with: "foo@bar.com"
+    fill_in "Password", with: "test"
+    fill_in "Password confirmation", with: "fest"
+    within(".well") do
+      click_on("Sign Up")
+    end
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+end
