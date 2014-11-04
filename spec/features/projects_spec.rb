fbe6c753cccf0552c64e2061f7f3ae9797d061cb
pclowes <peter.d.clowes@gmail.com>
2014-11-04 15:13:17 -0700

    added capybara to projects

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,12 +1,38 @@
 require 'rails_helper'
 
 feature "Projects" do
-  scenario "User creates a project"
+  scenario "User creates a project" do
     visit projects_path
-    expect(page).to have_no_content("Project")
+    expect(page).to have_no_content("Foo")
     click_on "Create Project"
-    fill_in "Name", with: "Project"
+    fill_in "Name", with: "Foo"
     click_on "Create Project"
-    expect(page).to have_content("Project")
+    expect(page).to have_content("Foo")
+  end
+
+  scenario "User edits a project" do
+    visit projects_path
+    expect(page).to have_no_content("Foo")
+    click_on "Create Project"
+    fill_in "Name", with: "Foo"
+    click_on "Create Project"
+    expect(page).to have_content("Foo")
+    click_on "Edit"
+    fill_in "Name", with: "Bar"
+    click_on "Update Project"
+    expect(page).to have_content("Bar")
+    expect(page).to have_no_content("Foo")
+  end
+
+  scenario "User destroys a project" do
+    visit projects_path
+    expect(page).to have_no_content("Foo")
+    click_on "Create Project"
+    fill_in "Name", with: "Foo"
+    click_on "Create Project"
+    expect(page).to have_content("Foo")
+    click_on "Destroy"
+    expect(page).to have_no_content("Foo")
+  end
 
 end
