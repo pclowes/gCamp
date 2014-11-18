687968b99c2ed4fd7bfa86d8aef00c2e8ee1f138
pclowes <peter.d.clowes@gmail.com>
2014-11-18 16:52:34 -0700

    added notices for membership deletion

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -30,7 +30,7 @@ class MembershipsController < ApplicationController
   def destroy
     membership = Membership.find(params[:id])
     membership.destroy
-    redirect_to project_memberships_path
+    redirect_to project_memberships_path, notice:  " #{membership.user.full_name} was removed successfully."
   end
 
   private
