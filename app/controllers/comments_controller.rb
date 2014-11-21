90bcc9dce54c2333ba307a145b9595442afe443c
pclowes <peter.d.clowes@gmail.com>
2014-11-21 14:07:59 -0700

    Fixed tasks show page formatting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,13 +5,15 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(params.require(:comment).permit(:comment, :task_id, :user_id).merge(:task_id => params[:task_id]))
+    @comment = Comment.new(params.require(:comment).permit(:comment, :task_id, :user_id)
+      .merge(:task_id => params[:task_id]))
     @comment.save
     redirect_to project_task_path(@project, @task)
   end
 
   def update
-    Comment.find(params[:id]).update(params.require(:comment).permit(:comment, :task_id, :user_id)).merge(:task_id => params[:task_id]))
+    Comment.find(params[:id]).update(params.require(:comment).permit(:comment, :task_id, :user_id)
+    .merge(:task_id => params[:task_id]))
     redirect_to project_task_path(@project, @task)
   end
 
