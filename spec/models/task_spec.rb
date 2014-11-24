c8e2d9eab69375dac020923a22756a0154526ed9
pclowes <peter.d.clowes@gmail.com>
2014-11-24 14:07:43 -0700

    fixed task model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,12 +4,12 @@ describe Task do
   it "saves a task w/o due date" do
     task = Task.new(description: 'foo')
     task.save
-    expect(task.valid?).to eq(true)
+    expect(task.errors[:due_date].present?).to eq(false)
   end
 
   it "does not save a task w/ due date in the past" do
-    task = Task.create(description: 'foo', due_date: Date.yesterday.strftime("%m/%d/%Y").to_s)
-    expect(task.valid?).to eq(false)
+    task = Task.create(description: 'foo', due_date: '11/11/2010')
+    expect(task.errors[:due_date].present?).to eq(true)
   end
 
 
@@ -19,10 +19,10 @@ describe Task do
     travel_to 1.year.ago do
       task.description= 'foobar'
       task.due_date= Date.today
-      expect(task.valid?).to eq(true)
+      expect(task.errors[:due_date].present?).to eq(false)
       task.save
     end
     task.description = 'oofrab'
-    expect(task.valid?).to eq(true)
+    expect(task.errors[:due_date].present?).to eq(false)
   end
 end
