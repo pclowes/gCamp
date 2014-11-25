ff47abb3f4154c50b3f9db4b3b92b45c53a076c2
pclowes <peter.d.clowes@gmail.com>
2014-11-25 14:43:52 -0700

    added dependent destroy and nullify to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :projects, through: :memberships
-  has_many :comments
+  has_many :comments, dependent: :nullify
   def full_name
     "#{first_name} #{last_name}"
   end
