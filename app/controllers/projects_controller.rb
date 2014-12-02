18d627feafe9f307f6b47d74bba0630af493aee4
pclowes <peter.d.clowes@gmail.com>
2014-12-01 18:45:01 -0700

    users redirected to project tasks page after project creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,7 +16,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     if @project.save
-      redirect_to project_path(@project), notice: 'Project was successfully created.'
+      redirect_to project_tasks_path(@project), notice: 'Project was successfully created.'
       Membership.create(project_id: @project.id, user_id: current_user.id, title: "Owner")
     else
       render :new
