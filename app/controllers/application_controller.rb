fab3fb2e49158d2651ad82d810c6dbf725e5747f
pclowes <peter.d.clowes@gmail.com>
2014-12-04 22:16:08 -0700

    added redirect after logging in from internal pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
 
-  def require_login
+  def require_login(return_point = request.url)
     unless current_user
+      set_return_point(return_point)
       redirect_to sign_in_path, notice: "You must be logged in to access that action"
     end
   end
+
+  def return_point
+    session[:return_point] || projects_path
+  end
+
+  def set_return_point(path, overwrite = false)
+    if overwrite or session[:return_point].blank?
+      session[:return_point] = path
+    end
+  end
+
+
 
   class AccessDenied < StandardError
   end
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,7 @@ class AuthenticationController < MarketingController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to projects_path
+      redirect_to return_point
     else
       @sign_in_error = "Username / password combination is invalid"
       render :new
