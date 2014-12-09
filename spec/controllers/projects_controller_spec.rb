f124bded0815dfb0fa750d79d6948fa096c2fddd
pclowes <peter.d.clowes@gmail.com>
2014-12-09 15:43:30 -0700

    controller spec for project edit permissions

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -27,7 +27,7 @@ describe ProjectsController do
       session[:user_id] = @user.id
       count = @projects.count
 
-      delete :destroy, id: @project.id
+      get :destroy, id: @project.id
 
       expect(response.status).to eq(404)
       expect(count).to eq(@projects.count)
@@ -37,7 +37,7 @@ describe ProjectsController do
       session[:user_id] = @user.id
       count = Project.count
 
-      delete :destroy, id: @project.id
+      get :destroy, id: @project.id
 
       expect(response.status).to eq(404)
       expect(count).to eq(@projects.count)
@@ -52,7 +52,7 @@ describe ProjectsController do
       session[:user_id] = @user.id
       count = Project.count
 
-      delete :destroy, id: @project.id
+      get :destroy, id: @project.id
       expect(@projects.count).to eq(count -1)
     end
 
@@ -72,8 +72,85 @@ describe ProjectsController do
       session[:user_id] = @user.id
       count = @projects.count
 
-      delete :destroy, id: @project.id
+      get :destroy, id: @project.id
       expect(@projects.count).to eq(count -1)
     end
   end
+
+
+  describe "#edit" do
+
+    before do
+      @user = User.create!(
+      first_name: "Joe",
+      last_name: "Smith",
+      email: "joe@example.com",
+      password: "test",
+      admin: false
+      )
+      @project = Project.create!(
+      name: "Acme"
+      )
+      @projects = Project.all
+    end
+
+    it "does not allow visitors to edit and redirects to sign-in page" do
+      get :edit, id: @project.id
+      expect(response.status).to redirect_to(signin_path)
+    end
+
+    it "does not allow non-members to edit" do
+      session[:user_id] = @user.id
+      name = @projects.name
+
+      get :edit, id: @project.id
+
+      expect(response.status).to eq(404)
+      expect(name).to eq(@projects.name)
+    end
+
+    it "does not allow project-members to edit" do
+      session[:user_id] = @user.id
+      name = Project.name
+
+      get :edit, id: @project.id
+
+      expect(response.status).to eq(404)
+      expect(name).to eq(@projects.name)
+    end
+
+    it "allows project owners to edit" do
+      Membership.create!(
+      user: @user,
+      project: @project,
+      title: 'Owner'
+      )
+      session[:user_id] = @user.id
+      name = Project.name
+
+      get :edit, id: @project.id
+      expect(response.status).to eq(200)
+    end
+
+    it "allows admins to edit" do
+      @user = User.create!(
+      first_name: "Jane",
+      last_name: "Doe",
+      email: "Jane@example.com",
+      password: "test",
+      admin: true
+      )
+      Membership.create!(
+      user: @user,
+      project: @project,
+      title: 'Owner'
+      )
+      session[:user_id] = @user.id
+      name = @projects.name
+
+      get :edit, id: @project.id
+      expect(response.status).to eq(200)
+    end
+  end
+
 end
